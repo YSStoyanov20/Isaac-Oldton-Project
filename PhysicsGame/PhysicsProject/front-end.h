f3fedf143f0bc3943f32837a6c5f514e4fa5a2e7
Petar <PDMatsaliev20@codingburgas.bg>
2021-11-23 17:11:56 +0200

    Calculator Design

--- PhysicsGame/PhysicsProject/front-end.h
+++ PhysicsGame/PhysicsProject/front-end.h
@@ -9,3 +9,23 @@ void homeScreen()
     std::cout << "  _| |_\\__ \\ (_| | (_| | (__  | |__| | | (_| | || (_) | | | |" << std::endl;
     std::cout << " |_____|___/\\__,_|\\__,_|\\___|  \\____/|_|\\__,_|\\__\\___/|_| |_|" << std::endl;
 }
+
+void CalculatorDesign()
+{
+        std::cout << " _____________________ " << std::endl;
+        std::cout << "|  _________________  |" << std::endl;
+        std::cout << "| |                 | |" << std::endl;
+        std::cout << "| |                 | |" << std::endl;
+        std::cout << "| |                 | |" << std::endl;
+        std::cout << "| |_________________| |" << std::endl;
+        std::cout << "|  ___ ___ ___   ___  |" << std::endl;
+        std::cout << "| | 7 | 8 | 9 | | + | |" << std::endl;
+        std::cout << "| |___|___|___| |___| |" << std::endl;
+        std::cout << "| | 4 | 5 | 6 | | - | |" << std::endl;
+        std::cout << "| |___|___|___| |___| |" << std::endl;
+        std::cout << "| | 1 | 2 | 3 | | x | |" << std::endl;
+        std::cout << "| |___|___|___| |___| |" << std::endl;
+        std::cout << "| | . | 0 | = | | / | |" << std::endl;
+        std::cout << "| |___|___|___| |___| |" << std::endl;
+        std::cout << "|_____________________|" << std::endl;
+}

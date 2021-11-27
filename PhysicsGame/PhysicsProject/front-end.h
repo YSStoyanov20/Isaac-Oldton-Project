c5ea8dc4053fc1297db528f4c02e7717c3224d5f
Petar <PDMatsaliev20@codingburgas.bg>
2021-11-27 19:10:55 +0200

    Removed Calculator Design

--- PhysicsGame/PhysicsProject/front-end.h
+++ PhysicsGame/PhysicsProject/front-end.h
@@ -2,30 +2,10 @@
 
 void homeScreen()
 {
-    std::cout << "  _____                         ____  _     _ _              " << std::endl;
-    std::cout << " |_   _|                       / __ \\| |   | | |             " << std::endl;
-    std::cout << "   | |  ___  __ _  __ _  ___  | |  | | | __| | |_ ___  _ __  " << std::endl;
-    std::cout << "   | | / __|/ _` |/ _` |/ __| | |  | | |/ _` | __/ _ \\| '_ \\ " << std::endl;
-    std::cout << "  _| |_\\__ \\ (_| | (_| | (__  | |__| | | (_| | || (_) | | | |" << std::endl;
-    std::cout << " |_____|___/\\__,_|\\__,_|\\___|  \\____/|_|\\__,_|\\__\\___/|_| |_|" << std::endl;
-}
-
-void CalculatorDesign()
-{
-        std::cout << " _____________________ " << std::endl;
-        std::cout << "|  _________________  |" << std::endl;
-        std::cout << "| |                 | |" << std::endl;
-        std::cout << "| |                 | |" << std::endl;
-        std::cout << "| |                 | |" << std::endl;
-        std::cout << "| |_________________| |" << std::endl;
-        std::cout << "|  ___ ___ ___   ___  |" << std::endl;
-        std::cout << "| | 7 | 8 | 9 | | + | |" << std::endl;
-        std::cout << "| |___|___|___| |___| |" << std::endl;
-        std::cout << "| | 4 | 5 | 6 | | - | |" << std::endl;
-        std::cout << "| |___|___|___| |___| |" << std::endl;
-        std::cout << "| | 1 | 2 | 3 | | x | |" << std::endl;
-        std::cout << "| |___|___|___| |___| |" << std::endl;
-        std::cout << "| | . | 0 | = | | / | |" << std::endl;
-        std::cout << "| |___|___|___| |___| |" << std::endl;
-        std::cout << "|_____________________|" << std::endl;
+    std::cout << "\t\t\t\t\t\t  _____                         ____  _     _ _              " << std::endl;
+    std::cout << "\t\t\t\t\t\t |_   _|                       / __ \\| |   | | |             " << std::endl;
+    std::cout << "\t\t\t\t\t\t   | |  ___  __ _  __ _  ___  | |  | | | __| | |_ ___  _ __  " << std::endl;
+    std::cout << "\t\t\t\t\t\t   | | / __|/ _` |/ _` |/ __| | |  | | |/ _` | __/ _ \\| '_ \\ " << std::endl;
+    std::cout << "\t\t\t\t\t\t  _| |_\\__ \\ (_| | (_| | (__  | |__| | | (_| | || (_) | | | |" << std::endl;
+    std::cout << "\t\t\t\t\t\t |_____|___/\\__,_|\\__,_|\\___|  \\____/|_|\\__,_|\\__\\___/|_| |_|" << std::endl;
 }
